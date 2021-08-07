96aa6a96c0e64cf0da15d2e1518b8a172dfbc84d
Kendrick Ang <kendrick.wh@outlook.com>
2021-08-07 16:26:09 +0800

    feat: add signup page and signup navbar link

--- main.go
+++ main.go
@@ -8,8 +8,11 @@ import (
 	"lenslocked.com/views"
 )
 
-var homeView *views.View
-var contactView *views.View
+var (
+	homeView    *views.View
+	contactView *views.View
+	signupView  *views.View
+)
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
@@ -19,6 +22,11 @@ func home(w http.ResponseWriter, r *http.Request) {
 func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	must(contactView.Render(w, nil))
+}
+
+func signup(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	must(signupView.Render(w, nil))
 }
 
 // A helper function that panics on any error
@@ -32,9 +40,11 @@ func main() {
 	// init templates
 	homeView = views.NewView("bootstrap", "views/home.gohtml")
 	contactView = views.NewView("bootstrap", "views/contact.gohtml")
+	signupView = views.NewView("bootstrap", "views/signup.gohtml")
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/signup", signup)
 	log.Fatal(http.ListenAndServe(":3000", r))
 }
