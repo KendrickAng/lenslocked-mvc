e0a8dc67ae1364b29c71487ca88aad5698c07af1
Kendrick Ang <kendrick.wh@outlook.com>
2021-08-07 11:10:17 +0800

    initial commit

---
+++ exp/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"html/template"
+	"os"
+)
+
+func main() {
+	t, err := template.ParseFiles("hello.gohtml")
+	if err != nil {
+		panic(err)
+	}
+
+	data := struct {
+		Name string
+		Email string
+		Friends []string
+	}{
+		Name: "John Smith",
+		Email: "john@smith.com",
+		Friends: []string{"Alice", "Bob", "Charlie"},
+	}
+
+	err = t.Execute(os.Stdout, data)
+	if err != nil {
+		panic(err)
+	}
+}
