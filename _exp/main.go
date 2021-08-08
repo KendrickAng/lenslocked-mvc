b701f11189c3496dfab07c3beed33fa866deac77
Kendrick Ang <kendrick.wh@outlook.com>
2021-08-08 16:06:32 +0800

    chore: learn jinzhu gorm

--- _exp/main.go
+++ _exp/main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
 
-	_ "github.com/lib/pq"
+	"github.com/jinzhu/gorm"
+
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
 const (
@@ -15,25 +16,51 @@ const (
 	dbname   = "calhounio_demo"
 )
 
+type User struct {
+	gorm.Model
+	Name  string
+	Email string `gorm:"not null;unique_index"`
+	Orders []Order
+}
+
+type Order struct {
+	gorm.Model
+	UserID      uint
+	Amount      int
+	Description string
+}
+
 func main() {
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
-	db, err := sql.Open("postgres", psqlInfo)
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := gorm.Open("postgres", psqlInfo)
 	if err != nil {
 		panic(err)
 	}
+	defer db.Close()
 
-	err = db.Ping()
-	if err != nil {
-		panic(err)
+	db.LogMode(true)
+	db.AutoMigrate(&User{}, &Order{})
+
+	var user User
+	db.Preload("Orders").First(&user)
+	if db.Error != nil {
+		panic(db.Error)
 	}
 
-	var id int
-	row := db.QueryRow(`INSERT INTO users(name, email) VALUES($1, $2) RETURNING id`, "Jon Calhoun", "jon@calhoun.io")
-	err = row.Scan(&id)
-	if err != nil {
-		panic(err)
-	}
+	fmt.Println("Email:", user.Email)
+	fmt.Println("Number of orders:", len(user.Orders))
+	fmt.Println("Orders:", user.Orders)
+}
 
-	fmt.Println("Successfully connected!")
-	db.Close()
+func createOrder(db *gorm.DB, user User, amount int, desc string) {
+	db.Create(&Order{
+		UserID:      user.ID,
+		Amount:      amount,
+		Description: desc,
+	})
+	if db.Error != nil {
+		panic(db.Error)
+	}
 }
