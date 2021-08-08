f4ba9d14ae44731e0b06c1aa6b9020974e48a6ff
Kendrick Ang <kendrick.wh@outlook.com>
2021-08-08 17:46:38 +0800

    feat: add user model

--- _exp/main.go
+++ _exp/main.go
@@ -3,9 +3,7 @@ package main
 import (
 	"fmt"
 
-	"github.com/jinzhu/gorm"
-
-	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"lenslocked.com/models"
 )
 
 const (
@@ -13,54 +11,41 @@ const (
 	port     = 5432
 	user     = "postgres"
 	password = "password"
-	dbname   = "calhounio_demo"
+	dbname   = "lenslocked_exp"
 )
-
-type User struct {
-	gorm.Model
-	Name  string
-	Email string `gorm:"not null;unique_index"`
-	Orders []Order
-}
-
-type Order struct {
-	gorm.Model
-	UserID      uint
-	Amount      int
-	Description string
-}
 
 func main() {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
 		"password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname)
-	db, err := gorm.Open("postgres", psqlInfo)
+	us, err := models.NewUserService(psqlInfo)
 	if err != nil {
 		panic(err)
 	}
-	defer db.Close()
+	defer us.Close()
+	us.DestructiveReset()
 
-	db.LogMode(true)
-	db.AutoMigrate(&User{}, &Order{})
-
-	var user User
-	db.Preload("Orders").First(&user)
-	if db.Error != nil {
-		panic(db.Error)
+	// Create a user
+	user := models.User{
+		Name:  "Michael Scott",
+		Email: "michael@dundermifflin.com",
+	}
+	if err := us.Create(&user); err != nil {
+		panic(err)
 	}
 
-	fmt.Println("Email:", user.Email)
-	fmt.Println("Number of orders:", len(user.Orders))
-	fmt.Println("Orders:", user.Orders)
-}
-
-func createOrder(db *gorm.DB, user User, amount int, desc string) {
-	db.Create(&Order{
-		UserID:      user.ID,
-		Amount:      amount,
-		Description: desc,
-	})
-	if db.Error != nil {
-		panic(db.Error)
+	// NOTE: You may need to update the query code a bit as well
+	// Update a user
+	user.Name = "Updated Name"
+	if err := us.Update(&user); err != nil {
+		panic(err)
 	}
+
+	foundUser, err := us.ByEmail("michael@dundermifflin.com")
+	if err != nil {
+		panic(err)
+	}
+	// Because of an update, the name should now
+	// be "Updated Name"
+	fmt.Println(foundUser)
 }
