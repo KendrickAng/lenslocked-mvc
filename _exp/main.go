c02f452e50f12c6dd011ca9881b23b207db61c56
Kendrick Ang <kendrick.wh@outlook.com>
2021-08-08 13:19:40 +0800

    chore: test connection to postgres db

--- _exp/main.go
+++ _exp/main.go
@@ -1,28 +1,39 @@
 package main
 
 import (
-	"html/template"
-	"os"
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "password"
+	dbname   = "calhounio_demo"
 )
 
 func main() {
-	t, err := template.ParseFiles("hello.gohtml")
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
 		panic(err)
 	}
 
-	data := struct {
-		Name    string
-		Email   string
-		Friends []string
-	}{
-		Name:    "John Smith",
-		Email:   "john@smith.com",
-		Friends: []string{"Alice", "Bob", "Charlie"},
-	}
-
-	err = t.Execute(os.Stdout, data)
+	err = db.Ping()
 	if err != nil {
 		panic(err)
 	}
+
+	var id int
+	row := db.QueryRow(`INSERT INTO users(name, email) VALUES($1, $2) RETURNING id`, "Jon Calhoun", "jon@calhoun.io")
+	err = row.Scan(&id)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully connected!")
+	db.Close()
 }
